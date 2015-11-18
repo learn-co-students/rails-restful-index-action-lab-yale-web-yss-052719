d30890b5efcda2a9e4bdc6a1dc3f704616414278
Jordan Hudgens <jordan@wow.com>
2015-11-18 14:32:13 -0600

    Added additional test and updated instructions

--- spec/features/student_index_spec.rb
+++ spec/features/student_index_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+feature 'Route to view' do
+  scenario 'has an index page' do
+    visit students_path
+
+    expect(page.status_code).to eq(200)
+  end
+end
+
 feature 'Multiple students are shown' do
   scenario 'on the index page' do
     Student.create!(first_name: "Daenerys", last_name: "Targaryen")
